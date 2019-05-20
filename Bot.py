7ff2280c27ded7fc70e424d9121b0bd3e7804f0f
Antti Huttunen <antti.t.huttunen@aalto.fi>
2019-05-20 11:16:23 +0300

    Problems fixed and bot is ready for action

--- Bot.py
+++ Bot.py
@@ -3,48 +3,55 @@ Created on 22.2.2019
 
 @author: Antti
 '''
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
+from telegram.ext import Updater, CommandHandler
 import logging
-import cv2
+from cv2 import VideoCapture, imwrite
 from PIL import Image
 from io import BytesIO
+'''
+Bot is for taking pictures of the coffee machine at Inkubio guildroom and sending them to tg.
+'''
 
+'''
+#Code for disasters
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
+'''
 
-
-def fuksi_stalk(bot, update):
-    
-    camera = cv2.VideoCapture(0)
+def kahvi_stalk(bot, update):
+    '''
+    Function takes image and sends image to tg after command kahvi
+    '''
+    camera = VideoCapture(0)
     return_value, image = camera.read()
-    cv2.imwrite('opencv'+str("spagu")+'.png', image)
+    imwrite('opencv'+str("spagu")+'.png', image)
     camera.release()
-    
+
     image = Image.open('opencvspagu.png')
-    '''
-    image.show()
-    '''
+   
     chat_id = update.message.chat_id
-    '''
-    bot.send_photo(chat_id=chat_id, photo=open('spagu/laah_puuh.jpeg', 'rb'))
-    '''
+    
     bio = BytesIO()
     bio.name = 'opencvspagu.png'
     image.save(bio, 'PNG')
     bio.seek(0)
     bot.send_photo(chat_id, photo=bio)
     
+    
 def start(bot, update):
+    '''
+    Function sends str to tg after command start
+    '''
+
     chat_id = update.message.chat_id
-    bot.send_message(chat_id=chat_id, text="En tieda onko kahvia")
+    bot.send_message(chat_id=chat_id, text="Kerron sinulle, onko kiltahuoneella kahvia")
     
 def main():
     updater = Updater(token='Token')
     dispatcher = updater.dispatcher
     
     start_handler = CommandHandler('start', start)
-    stalk_handler = CommandHandler('fuksi_stalk', fuksi_stalk)
+    stalk_handler = CommandHandler('kahvi', kahvi_stalk)
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(stalk_handler)
 
